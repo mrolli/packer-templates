9d937b9152de421cdf5c60d0e25b51372b3f19f6
Michael Rolli <michael@rollis.ch>
2014-06-09 08:50:02 +0200

    Puppet bootstrapping now works for all Ubuntu releases.
    Previously precise was hardcoded in puppet bootstrap script. Now
    lsb-release information is honored to install the correct PuppetLabs
    repository.

--- scripts/ubuntu/puppet.sh
+++ scripts/ubuntu/puppet.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
+#
+# This bootstraps Puppet on Ubuntu
+#
+set -e
 
-# Prepare puppetlabs repo
-wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
-dpkg -i puppetlabs-release-precise.deb
-rm -f pupetlabs-release-precise.deb
-apt-get update
+# Load up the release information
+. /etc/lsb-release
+REPO_DEB_URL="http://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb"
+
+# Install the PuppetLabs repository
+echo "Configuring PuppetLabs repo..."
+REPO_DEB_PATH=$(mktemp)
+wget --output-document="${REPO_DEB_PATH}" "${REPO_DEB_URL}" 2>/dev/null
+dpkg -i "${REPO_DEB_PATH}" >/dev/null
+rm -f $REPO_DEB_PATH >/dev/null
+apt-get update >/dev/null
 
 # Install puppet stuff
-apt-get install -y puppet hiera facter
+apt-get install -y puppet hiera facter >/dev/null
+echo "Puppet installed!"
 
 exit 0
