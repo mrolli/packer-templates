f16ce70ea0798402341133227d87664043636456
Rolli, Michael (ID) <michael.rolli@id.unibe.ch>
2015-08-28 09:59:59 +0200

    Small deployment script to copy boxes from build/ to an rsync destination.

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+BUILD_DIR="build"
+BUILD_FILE_PREFIX="built-for-virtualbox"
+BUILD_FILE=$BUILD_DIR/$BUILD_FILE_PREFIX-`VBoxMAnage --version`.txt
+
+if [ -z $1 ]; then
+  echo "USAGE: $0 <rsync_destination>"
+  exit 1
+fi
+
+DESTINATION=$1
+
+# Delete old build file
+rm -f ${BUILD_DIR}/${BUILD_FILE_PREFIX}-*
+# Generate build file
+for BOX in $BUILD_DIR/*.box; do
+  BOX_CTIME=$(stat -Lf "%Sm" -t "%Y-%m-%dT%H:%M:%S" $BOX)
+  echo "${BOX_CTIME}: ${BOX}" >> $BUILD_FILE
+done
+
+# Transfer stuff to destination
+rsync -av \
+  --exclude=".gitkeep" \
+  --delete \
+  --stats \
+  --progress \
+  $BUILD_DIR/ $DESTINATION/
+
+exit 0
+
